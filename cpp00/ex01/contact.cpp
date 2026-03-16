071fbb660647123f7aac7f720264685d1df99d25
Fiona-0525 <jiyanw825@gmail.com>
2026-03-16 21:03:09 +0100

    add

--- cpp00/ex01/contact.cpp
+++ cpp00/ex01/contact.cpp
@@ -1,41 +1,41 @@
 #include "contact.hpp"
 
-void Contact::set_first_name(const std::string_view first_name){
-    this->first_name = first_name;
+void Contact::set_first_name(const std::string_view _first_name){
+    this->_first_name = _first_name;
 }
 
-void Contact::set_last_name(const std::string_view last_name){
-    this->last_name = last_name;
+void Contact::set_last_name(const std::string_view _last_name){
+    this->_last_name = _last_name;
 }
 
-void Contact::set_nickname(const std::string_view nickname){
-    this->nickname = nickname;
+void Contact::set_nickname(const std::string_view _nickname){
+    this->_nickname = _nickname;
 }
 
-void Contact::set_phone_number(const std::string_view phone_number){
-    this->phone_number = phone_number;
+void Contact::set_phone_number(const std::string_view _phone_number){
+    this->_phone_number = _phone_number;
 }
 
-void Contact::set_darkest_secret(const std::string_view darkest_secret){
-    this->darkest_secret = darkest_secret;
+void Contact::set_darkest_secret(const std::string_view _darkest_secret){
+    this->_darkest_secret = _darkest_secret;
 }
 
 std::string Contact::get_first_name(void) const{
-    return this->first_name;
+    return this->_first_name;
 }
 
 std::string Contact::get_last_name(void) const{
-    return this->last_name;
+    return this->_last_name;
 }
 
 std::string Contact::get_nickname(void) const{
-    return this->nickname;
+    return this->_nickname;
 }
 
 std::string Contact::get_phone_number(void) const{
-    return this->phone_number;
+    return this->_phone_number;
 }
 
 std::string Contact::get_darkest_secret(void) const{
-    return this->darkest_secret;
+    return this->_darkest_secret;
 }
