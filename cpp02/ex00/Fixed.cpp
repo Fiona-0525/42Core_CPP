aea148377559219495a492a86892e631434b26db
Fiona-0525 <jiyanw825@gmail.com>
2026-04-22 21:55:08 +0200

    modif

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,18 +1,18 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed() : _rawBits(0) {
+Fixed::Fixed() : _value(0) {
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &other) {
+Fixed::Fixed(const Fixed &other) : _value(other._value) {
     std::cout << "Copy constructor called" << std::endl;
-    _rawBits = other.getRawBits();
 }
 
 Fixed &Fixed::operator=(const Fixed &other) {
     std::cout << "Copy assignment operator called" << std::endl;
-    if (this != &other)
-        _rawBits = other.getRawBits();
+    if (this != &other) {
+        this->_value = other.getRawBits();
+    }
     return *this;
 }
 
@@ -20,12 +20,11 @@ Fixed::~Fixed() {
     std::cout << "Destructor called" << std::endl;
 }
 
-void Fixed::setRawBits(int const raw) {
-    std::cout << "setRawBits member function called" << std::endl;
-    _rawBits = raw;
-}
 int Fixed::getRawBits(void) const {
     std::cout << "getRawBits member function called" << std::endl;
-    return _rawBits;
+    return this->_value;
 }
 
+void Fixed::setRawBits(int const raw) {
+    this->_value = raw;
+}
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -3,18 +3,18 @@
 # include <iostream>
 
 class Fixed {
-private:
-    int              _rawBits = 0;
-    static const int _fractionalBits = 8;
-
 public:
     Fixed();
     ~Fixed();
     Fixed(const Fixed &other);
     Fixed &operator=(const Fixed &other);
-
+    
     void setRawBits(int const raw);
     int  getRawBits(void) const;
+
+private:
+    int              _value = 0;
+    static const int bits_ = 8;
 };
 
 #endif
