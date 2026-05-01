200656e58b059f0506f0f8ad7cce6990c1c94721
Fiona-0525 <jiyanw825@gmail.com>
2026-05-01 21:53:03 +0200

    updat0105

--- cpp04/ex00/Cat.cpp
+++ cpp04/ex00/Cat.cpp
@@ -1,2 +1,30 @@
 #include "Cat.hpp"
 
+Cat::Cat()
+{
+    this->type = "🐱 Cat";
+    std::cout << "🐱 Cat constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat &copy) : Animal(copy)
+{
+    std::cout << "🐱 Cat copy constructor called" << std::endl;
+}
+
+Cat &Cat::operator=(const Cat &copy)
+{
+    std::cout << "🐱 Cat copy assignment operator called" << std::endl;
+    if (this != &copy)
+        Animal::operator=(copy);
+    return *this;
+}
+
+Cat::~Cat()
+{
+    std::cout << "🐱 Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+    std::cout << "🐱 Meow!" << std::endl;
+}
